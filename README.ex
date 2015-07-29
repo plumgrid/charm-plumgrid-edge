dbcea1f093a45eb89ba29f0a32514f6ab79a6f57
Bilal Baqar <bbaqar@plumgrid.com>
2015-07-29 11:19:18 -0700

    Addressed reviews by Charmers

--- README.ex
+++ README.ex
@@ -4,41 +4,41 @@ This charm is responsible for configuring a Compute node as a PLUMgrid Edge.
 
 Once deployed, the charm configures the IO Visor kernel module as a PLUMgrid Edge. The charm also triggers the services essential for a PLUMgrid Edge.
 
+It is a subordinate charm to nova-compute.
+
 # Usage
 
 Step by step instructions on using the charm:
 
     juju deploy neutron-api
-    juju deploy neutron-plumgrid-plugin neutron-api
-    juju deploy neutron-iovisor
-    juju deploy plumgrid-director --to <Machince No of neutron-iovisor>
-    juju add-unit neutron-iovisor
-    juju deploy plumgrid-edge --to <Machice No of 2nd unit of neutron-iovisor>
+    juju deploy neutron-api-plumgrid
+    juju deploy plumgrid-director
+    juju deploy nova-compute
+    juju deploy plumgrid-edge
 
-    juju add-relation neutron-api neutron-plumgrid-plugin
-    juju add-relation neutron-plumgrid-plugin neutron-iovisor
-    juju add-relation neutron-iovisor plumgrid-director
-    juju add-relation neutron-iovisor plumgrid-edge
+    juju add-relation neutron-api neutron-api-plumgrid
+    juju add-relation neutron-api-plumgrid plumgrid-director
     juju add-relation plumgrid-director plumgrid-edge
+    juju add-relation nova-compute plumgrid-edge
 
-For plumgrid-edge to work make the configuration in the neutron-api, neutron-plumgrid-plugin, neutron-iovisor and plumgrid-director charms as specified in the configuration section below.
+For plumgrid-edge to work make the configuration in the neutron-api, neutron-api-plumgrid and plumgrid-director charms as specified in the configuration section below.
 
 # Known Limitations and Issues
 
-This is an early access version of the PLUMgrid Edge charm and it is not meant for production deployments. The charm currently only supports JUNO. This charm needs to be deployed on a node where a unit of neutron-iovisor charm exists. Also plumgrid-director and plumgrid-gateway charms should not be deployed on the same node.
+This is an early access version of the PLUMgrid Edge charm and it is not meant for production deployments. The charm currently only supports Kilo Openstack Release.
 
 # Configuration
 
-plumgrid-edge charm does not require any configuration itself but the following config is required in the other charms.
-
 Example Config
 
-    plumgrid-director:
-        plumgrid-virtual-ip: "192.168.100.250"
-    neutron-iovisor:
+    plumgrid-edge:
         install_sources: 'ppa:plumgrid-team/stable'
         install_keys: 'null'
-    neutron-plumgrid-plugin:
+    plumgrid-director:
+        plumgrid-virtual-ip: "192.168.100.250"
+        install_sources: 'ppa:plumgrid-team/stable'
+        install_keys: 'null'
+    neutron-api-plumgrid:
         install_sources: 'ppa:plumgrid-team/stable'
         install_keys: 'null'
         enable-metadata: False
@@ -46,6 +46,7 @@ Example Config
         neutron-plugin: "plumgrid"
         plumgrid-virtual-ip: "192.168.100.250"
 
+Provide the source repo path for PLUMgrid Debs in 'install_sources' and the corresponding keys in 'install_keys'.
 The virtual IP passed on in the neutron-api charm has to be same as the one passed in the plumgrid-director charm.
 
 # Contact Information
--- hooks/pg_edge_context.py
+++ hooks/pg_edge_context.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2015, PLUMgrid Inc, http://plumgrid.com
+
+# This file contains the class that generates context for PLUMgrid template files.
+
 from charmhelpers.core.hookenv import (
     relation_ids,
     related_units,
@@ -7,36 +11,10 @@ from charmhelpers.contrib.openstack import context
 
 from socket import gethostname as get_unit_hostname
 
-'''
-#This function will be used to get information from neutron-api
-def _neutron_api_settings():
-    neutron_settings = {
-        'neutron_security_groups': False,
-        'l2_population': True,
-        'overlay_network_type': 'gre',
-    }
-    for rid in relation_ids('neutron-plugin-api'):
-        for unit in related_units(rid):
-            rdata = relation_get(rid=rid, unit=unit)
-            if 'l2-population' not in rdata:
-                continue
-            neutron_settings = {
-                'l2_population': rdata['l2-population'],
-                'neutron_security_groups': rdata['neutron-security-groups'],
-                'overlay_network_type': rdata['overlay-network-type'],
-            }
-            # Override with configuration if set to true
-            if config('disable-security-groups'):
-                neutron_settings['neutron_security_groups'] = False
-            return neutron_settings
-    return neutron_settings
-'''
 
-
-#Use this function to get information from the director
 def _pg_dir_settings():
     '''
-    Inspects current plumgrid relation
+    Inspects relation with PLUMgrid director.
     '''
     pg_settings = {
         'pg_dir_ip': '192.168.100.201',
@@ -51,35 +29,40 @@ def _pg_dir_settings():
 
 
 class PGEdgeContext(context.NeutronContext):
-    interfaces = []
 
     @property
     def plugin(self):
+        '''
+        Over-riding function in NeutronContext Class to return 'plumgrid'
+        as the neutron plugin.
+        '''
         return 'plumgrid'
 
     @property
     def network_manager(self):
+        '''
+        Over-riding function in NeutronContext Class to return 'neutron'
+        as the network manager.
+        '''
         return 'neutron'
 
     def _save_flag_file(self):
+        '''
+        Over-riding function in NeutronContext Class.
+        Function only needed for OVS.
+        '''
         pass
 
-    #@property
-    #def neutron_security_groups(self):
-    #    neutron_api_settings = _neutron_api_settings()
-    #    return neutron_api_settings['neutron_security_groups']
-
     def pg_ctxt(self):
-        #Generated Config for all Plumgrid templates inside
-        #the templates folder
+        '''
+        Generated Config for all PLUMgrid templates inside the templates folder.
+        '''
         pg_ctxt = super(PGEdgeContext, self).pg_ctxt()
         if not pg_ctxt:
             return {}
 
-        #conf = config()
         pg_dir_settings = _pg_dir_settings()
         pg_ctxt['local_ip'] = pg_dir_settings['pg_dir_ip']
-        #neutron_api_settings = _neutron_api_settings()
         pg_ctxt['pg_hostname'] = "pg-edge"
         pg_ctxt['interface'] = "juju-br0"
         pg_ctxt['label'] = get_unit_hostname()
