0bc950e74afb2664e9353d27adfa4470299c61b1
Junaid Ali <junaidali@plumgrid.com>
2016-10-27 23:58:28 +0500

    Charmhelpers sync
        * Added osplatform in sync list
    
    Ticket: [SOL-1257]
    Change-Id: Ia975762640bfbe349b02462e5129ee269fdb46c1
    Signed-off-by: Junaid Ali <junaidali@plumgrid.com>

--- hooks/charmhelpers/contrib/openstack/exceptions.py
+++ hooks/charmhelpers/contrib/openstack/exceptions.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# Copyright 2014-2015 Canonical Limited.
+# Copyright 2016 Canonical Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,18 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
 
-__author__ = "Jorge Niedbalski <jorge.niedbalski@canonical.com>"
+class OSContextError(Exception):
+    """Raised when an error occurs during context generation.
 
-
-def current_version():
-    """Current system python version"""
-    return sys.version_info
-
-
-def current_version_string():
-    """Current system python version as string major.minor.micro"""
-    return "{0}.{1}.{2}".format(sys.version_info.major,
-                                sys.version_info.minor,
-                                sys.version_info.micro)
+    This exception is principally used in contrib.openstack.context
+    """
+    pass
